672fcf308b37563bf1f2357ef550bff73a940174
Jonathan Eid <jonathaneid98@gmail.com>
2022-05-15 01:07:40 -0400

    added swipe functionality

--- src/views/Swipe/Swipe.tsx
+++ src/views/Swipe/Swipe.tsx
@@ -1,17 +1,25 @@
 import React, { FC } from 'react'
 import Card from '../../components/Card'
 import DeckControls from '../../components/DeckControls'
+import TinderCard from 'react-tinder-card'
 
 
 interface Props {
     // any props that come into the component
 }
+
+
 const Swipe: FC<Props> = ({children, ...rest}) => {
 
-    return <div className="justify-content-center flex-column d-flex" style={{justifyContent: 'center', justifyItems: 'center', alignItems: 'center', position: 'relative'}} >
-                <Card>
-                </Card>
 
+    return <div className="justify-content-center flex-column d-flex" style={{justifyContent: 'center', justifyItems: 'center', alignItems: 'center', position: 'relative'}} >
+                <TinderCard
+
+                >
+                   <Card>
+
+                   </Card>
+                </TinderCard>
                 <DeckControls> 
                     
                 </DeckControls>
