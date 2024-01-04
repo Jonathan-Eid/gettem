1e6e40fe4b5a8ea36b75a26ad7c72ce18ae7099d
Jonathan Eid <jonathaneid98@gmail.com>
2024-01-04 21:14:43 +0900

    Added some strapi api axios calls

--- frontend/src/views/Swipe/Swipe.tsx
+++ frontend/src/views/Swipe/Swipe.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import Card from '../../components/Card'
 import DeckControls from '../../components/DeckControls'
 import TinderCard from 'react-tinder-card'
 import './Swipe.scss'
+import { getCards } from '../../api/strapi'
 
 
 interface Props {
@@ -11,6 +12,19 @@ interface Props {
 
 
 const Swipe: FC<Props> = ({children, ...rest}) => {
+
+
+    const [cards, setCards] = useState([])
+
+    useEffect( () => {
+  
+        getCards().then(data => {
+            console.log(data)
+            setCards(data.data)
+        })
+    
+    }, [])
+    
 
 
     const objs = [0,1,2,3,4,5,6]
