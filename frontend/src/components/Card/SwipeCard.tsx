e2e7bb5c28e0a1c89ea51ad74083a13d2bfc4b79
Jonathan Eid <jonathaneid98@gmail.com>
2024-01-23 18:25:35 -0500

    Got the carousel working

--- frontend/src/components/Card/SwipeCard.tsx
+++ frontend/src/components/Card/SwipeCard.tsx
@@ -44,16 +44,8 @@ const SwipeCard: FC<Props> = ({children,  ...props}) => {
  
             </Card>
 
-              {/* <Carousel>
-                  {card_images.map((image:any,i:number) => {
-                    <Carousel.Item>
-                      <img
-                        className="d-block w-100"
-                        src={"http://localhost:1337"+image.attributes.formats.large.url}
-                      />
-                    </Carousel.Item>
-                  })}
-                </Carousel> */}
+            
+            
             
 
             <Modal show={show} onHide={handleClose}>
@@ -62,6 +54,24 @@ const SwipeCard: FC<Props> = ({children,  ...props}) => {
             </Modal.Header>
 
               <Modal.Body>
+
+              
+              <Carousel className='carousel'> 
+              
+                  {card_images.map((image:any,i:number) => {
+
+                    return <Carousel.Item className="carousel-item"> 
+                    
+                      <img
+                          className="carousel-img"
+                          src={"http://localhost:1337"+image.attributes.formats.large.url}
+                      /> 
+
+                    
+                    </Carousel.Item>
+                  })} 
+                  
+                </Carousel>
                 
                 {card.description}
               </Modal.Body>
@@ -81,21 +91,3 @@ const SwipeCard: FC<Props> = ({children,  ...props}) => {
 } 
 
 export default SwipeCard  
-
- 
-{/* <div class="col-sm-4"><div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?tech,street');">
-<img class="card-img d-none" src="https://source.unsplash.com/600x900/?tech,street" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
-<div class="card-img-overlay d-flex flex-column">
-<div class="card-body">
-   <small class="card-meta mb-2">Thought Leadership</small>
-   <h4 class="card-title mt-0 "><a class="text-white" herf="#">Goverment Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-  <small><i class="far fa-clock"></i> October 15, 2020</small>
- </div>
- <div class="card-footer">
-  <div class="media">
-<img class="mr-3 rounded-circle" src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-512.png" alt="Generic placeholder image" style="max-width:50px">
-<div class="media-body">
-<h6 class="my-0 text-white d-block">Oz COruhlu</h6>
-<small>Director of UI/UX</small>
-</div>
-</div> */}
