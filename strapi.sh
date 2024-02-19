11288a06565562ded1a29a3b3618d4c26140edd1
Jonathan Eid <jonathaneid98@gmail.com>
2024-02-18 22:10:41 -0500

    Added strapi and react deployments

---
+++ strapi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export DB_USER=${DB_USER}
+export DB_PASS=${DB_PASS}
+
+cd ~/gettem
+
+git fetch origin prod
+
+git reset --hard FETCH_HEAD
+
+git clean -df
+
+docker compose -f docker-compose-prod.yml up -d traefik
