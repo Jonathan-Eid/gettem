f2ae0c55257cc2a671d4fb986411041bfa1140df
Jonathan Eid <jonathaneid98@gmail.com>
2024-02-19 20:59:52 -0500

    Serving with serve instead

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
       },
       {
         path: "swipe",
-        element: <Swipe />
+        element: <Swipe /> 
       },
       {
         path: "gallery",
