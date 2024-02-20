ed2c9005885b507cf340dbf8d988d359f40da5fa
Jonathan-Eid <jonathaneid98@gmail.com>
2024-02-19 19:01:51 -0500

    Update strapi.sh

--- strapi.sh
+++ strapi.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export DB_USER=$PG_USER
-export DB_PASS=$PG_PASS
+export DB_USER='${PG_USER}'
+export DB_PASS='${PG_PASS}'
 
-echo $DB_PASS
-echo $DB_USER
-echo $PG_USER
-echo $$PG_PASS
+echo "${DB_PASS}"
+echo '{$DB_USER}'
+echo '$PG_USER'
+echo '${PG_PASS}'
 
 
 cd ~/gettem
@@ -17,4 +17,4 @@ git reset --hard FETCH_HEAD
 
 git clean -df
 
-DB_USER=$PG_USER DB_PASS=$PG_PASS docker compose -f docker-compose-prod.yml up -d strapi
+DB_USER='${PG_USER}' DB_PASS='${PG_PASS}' docker compose -f docker-compose-prod.yml up -d strapi
